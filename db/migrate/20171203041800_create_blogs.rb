9c55ae255e6d7d15eb7950471eece9fc04de7724
ezoetsuyoshi <nanasinogonkun@yahoo.co.jp>
2017-12-05 18:04:14 +0900

    finish4-7

---
+++ db/migrate/20171203041800_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blogs do |t|
+
+      t.timestamps
+      t.string :title
+      t.string :category
+      t.text :body
+    end
+  end
+end
